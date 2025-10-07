2bc4604bfc2546158cb10265405fe623167ac8bd
ayush_ahirwar-12 <ayushahirwar04@gmail.com>
2025-10-07 20:02:44 +0530

    dsfj

--- server.js
+++ server.js
@@ -1,57 +1,39 @@
 require("dotenv").config();
-const express = require("express")
-const songRoutes = require("./src/routes/song.route")
+const express = require("express");
+const songRoutes = require("./src/routes/song.route");
 const cors = require("cors");
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 let isConnected = false;
-async function connectoMongoDb() 
-{
-    try {
-        await mongoose.connect(process.env.MONGOOSE_UR,{
-            useNewUrlParser:true,
-        useUnifiedTopology:true
-        });
-        isConnected=true;
-        console.log("connect to db");
-        
-        
-    } catch (error) {
-        console.log("error connecting to mongoDb",error);
-        
-    }
+async function connectoMongoDb() {
+  try {
+    await mongoose.connect(process.env.MONGOOSE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log("connect to db");
+  } catch (error) {
+    console.log("error connecting to mongoDb", error);
+  }
 }
 
-app.use((req,res,next)=>{
-    if(!isConnected){
-        connectoMongoDb();
-    }
-    next();
-})
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectoMongoDb();
+  }
+  next();
+});
 
-app.use("/",songRoutes);
-
-
-
-
-
-
-
-
-
-
-
+app.use("/", songRoutes);
 
 // app.listen(4000,()=>{
 //     console.log("server Connected");
-    
+
 // });
 
-module.exports=app;
+module.exports = app;
--- src/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-function connecdb(){
-    mongoose.connect(process.env.MONGOOSE_URI).then(()=>{
-        console.log("database connected");
-        
-    })
-}
-module.exports=connecdb
