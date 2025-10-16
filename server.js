dc847984f02292fc1fb39367f66c1410908df5de
ayush_ahirwar-12 <ayushahirwar04@gmail.com>
2025-10-16 11:39:23 +0530

    a

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const cookie = require("cookie-parser");
 const userRoutes = require("./src/routes/user.route");
-const connecDb = require("./src/db/db");
+// const connecDb = require("./src/db/db");
 
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://music-backend-weld.vercel.app"],
+    origin: ["http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
      allowedHeaders: ["Content-Type", "Authorization"]
@@ -41,12 +41,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/", songRoutes);
+app.use("/api", songRoutes);
 app.use("/api/auth", userRoutes);
 
-// app.listen(4000, () => {
-//   console.log("server Connected");
-//   // connecDb();
-// });
+app.listen(4000, () => {
+  console.log("server Connected");
+  // connecDb();
+});
 
 module.exports = app;
